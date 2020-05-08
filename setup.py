fd95f62fbd4b25b71c370db861c8310d1969db11
Christoph Rauch <smokey42@users.noreply.github.com>
2020-05-08 09:55:13 +0200

    Python 3.8

--- setup.py
+++ setup.py
@@ -7,16 +7,14 @@ try:
 except IOError:
     long_description = "python-paipa"
 
-
 setup(
     name='paipa',
-    version='0.3.3',
+    version='0.3.4',
     description='Python pipeline library. Maori: (noun) pipe.',
     long_description=long_description,
     packages=['paipa'],
     author='python-paipa contributors',
-    author_email='python-tribe@stylight.com',
-    url='https://github.com/stylight/python-paipa',
+    url='https://github.com/poppencorken/python-paipa',
     license='Apache Software License 2.0',
     setup_requires=[
         'setuptools>=18.0',
@@ -30,12 +28,10 @@ setup(
         'six',
         'enum34',
     ],
-    ext_modules=[
-        Extension(
-            'paipa.debugger',
-            sources=['paipa/debugger.py'],
-        )
-    ],
+    ext_modules=[Extension(
+        'paipa.debugger',
+        sources=['paipa/debugger.py'],
+    )],
     extras_require={
         'glue': ['tornado'],
     },
@@ -49,6 +45,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
